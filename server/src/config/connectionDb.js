f7bef1197b788f6008df0976fc993fb59f4103d3
Minh Nguyen <lkcminh@gmail.com>
2023-03-20 14:32:12 +0700

    migrations

--- server/src/config/connectionDb.js
+++ server/src/config/connectionDb.js
@@ -1,18 +1,18 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("phongtro123", "root", null, {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3307,
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
 });
 
 const connectionDb = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
 };
 
 module.exports = connectionDb;
